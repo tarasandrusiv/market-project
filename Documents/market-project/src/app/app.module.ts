7f2fe61a1d785917f722a704ff9784e08f0f7a07
NikolayBlackJack <nikola716@gmail.com>
2019-02-14 19:25:35 +0200

    new

--- Documents/market-project/src/app/app.module.ts
+++ Documents/market-project/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ListProductComponent } from './list-product/list-product.component';
+import { BasketComponent } from './basket/basket.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListProductComponent,
+    BasketComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
