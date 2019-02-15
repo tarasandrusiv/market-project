5570313a3cf4ef547fe0b485bc7af8256b9c39e3
FantomAS527249 <zelisko.mykhaylo@gmail.com>
2019-02-15 23:37:27 +0200

    Add all data and images in mock-cars
    Signed-off-by: FantomAS527249 <zelisko.mykhaylo@gmail.com>

--- Documents/market-project/src/app/mock-data/mock-cars.ts
+++ Documents/market-project/src/app/mock-data/mock-cars.ts
@@ -2,43 +2,123 @@ import { Car } from "../models/car";
 
 export const CARS : Car[] = [
     { 
-        id:30, 
-        model: 'Susuki Vitara', 
-        description: 'Двигун: 1.4 BOOSTERJET, Трансмісія: 6-ступенчатая автоматическая коробка передач, ALLGRIP 4WD: 1. Режим Auto; 2. Режим Sport, Освітлення: Денні ходові фари',
-        price: 449900,
+        id: 30, 
+        model: "Vitara", 
+        description: "some text",
+        price: 528000,
         photo: "./assets/Vitara.png",
         markId: 100
     },
     { 
-        id:31, 
-        model: 'Susuki SX4', 
-        description: 'ALLGRIP 4WD:Система повного привода , Трансмісія: 6-ступенчатая автомат, ALLGRIP 4WD: 1. Режим Auto; 2. Режим Sport',
-        price: 4493242,
+        id: 31, 
+        model: "SX4", 
+        description: "some text",
+        price: 488000,
         photo: "./assets/SX4.png",
         markId: 100
     },
     { 
-        id:32, 
-        model: 'Susuki VitaraV2', 
-        description: 'ALLGRIP 4WD:Система повного привода , Трансмісія: 6-ступенчатая автомат, ALLGRIP 4WD: 1. Режим Auto; 2. Режим Sport',
-        price: 528000,
-        photo: "./assets/VitaraV2.png",
+        id: 32, 
+        model: "Vitara V2", 
+        description: "some text",
+        price: 449000,
+        photo: "./assets/Vitara-V2.png",
         markId: 100
     },
     { 
-        id:33, 
-        model: 'Susuki VitaraV2', 
-        description: 'ALLGRIP 4WD:Система повного привода , Трансмісія: 6-ступенчатая автомат, ALLGRIP 4WD: 1. Режим Auto; 2. Режим Sport',
-        price: 528760,
-        photo: "./assets/VitaraS2.png",
+        id: 33, 
+        model: "Vitara S", 
+        description: "some text",
+        price: 658800,
+        photo: "./assets/Vitara-S.png",
         markId: 100
     },
     { 
-        id:32, 
-        model: 'Susuki VitaraV2', 
-        description: 'ALLGRIP 4WD:Система повного привода , Трансмісія: 6-ступенчатая автомат, ALLGRIP 4WD: 1. Режим Auto; 2. Режим Sport',
-        price: 528000,
+        id: 34, 
+        model: "Jimny", 
+        description: "some text",
+        price: 485000,
         photo: "./assets/Jimny.png",
         markId: 100
+    },
+    { 
+        id: 10, 
+        model: "Arteon", 
+        description: "some text", 
+        price: 1374841,
+        photo: "./assets/Arteon.png",
+        markId: 300
+    },
+    { 
+        id: 11, 
+        model: "Golf", 
+        description: "some text",
+        price: 579891,
+        photo: "./assets/Golf.png",
+        markId: 300
+    },
+    { 
+        id: 12, 
+        model: "Passat", 
+        description: "some text",
+        price: 878302,
+        photo: "./assets/Passat.png",
+        markId: 300
+    },
+    { 
+        id: 13, 
+        model: "Polo sedan", 
+        description: "some text",
+        price: 374679,
+        photo: "./assets/Polo-sedan.png",
+        markId: 300
+    },
+    { 
+        id: 14, 
+        model: "Touareg", 
+        description: "some text",
+        price: 1673219,
+        photo: "./assets/Touareg.png",
+        markId: 300
+    },
+    {
+        id: 50, 
+        model: "X5", 
+        description: "some text", 
+        price: 1797292, 
+        photo: "./assets/X5.png", 
+        markId: 200
+    },
+    {
+        id: 51, 
+        model: "M2", 
+        description: "some text", 
+        price: 1783311, 
+        photo: "./assets/M2.png", 
+        markId: 200
+    },
+    {
+        id: 52, 
+        model: "M240i", 
+        description: "some text", 
+        price: 1448037, 
+        photo: "./assets/M240i.png", 
+        markId: 200
+    },
+    {
+        id: 53, 
+        model: "Gran Tourer", 
+        description: "some text", 
+        price: 842038, 
+        photo: "./assets/Gran-Tourer.png", 
+        markId: 200
+    },
+    {
+        id: 54, 
+        model: "M140i", 
+        description: "some text", 
+        price: 1274712, 
+        photo: "./assets/M140i.png", 
+        markId: 200
     },
 ];
