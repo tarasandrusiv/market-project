3b4e26560c202a9f819f7a03cd0827849cb5d538
NikolayBlackJack <44206804+NikolayBlackJack@users.noreply.github.com>
2019-02-18 21:56:30 +0200

    total

--- Documents/market-project/src/app/chart/chart.component.ts
+++ Documents/market-project/src/app/chart/chart.component.ts
@@ -10,33 +10,72 @@ import { CARS } from '../mock-data/mock-cars';
 })
 export class ChartComponent implements OnInit {
   cars = CARS;
+  len = this.cars.length;
   arr: any;
-
+  total = 0;
+  order = [
+    {
+      total: this.total,
+      // quantity: this.arr.price,
+      // model: this.arr.model
+    }
+  ]
+  quantityNum = document.getElementById('quantity-num"');
+  quantityPlus = document.getElementsByClassName('quantity-arrow-plus');
+  quantityMin = document.getElementsByClassName('quantity-arrow-minus');
   constructor() { 
-    localStorage.setItem('car', JSON.stringify([{ 
-      id:32, 
-      model: 'Susuki VitaraV2', 
-      description: 'ALLGRIP 4WD:Система повного привода , Трансмісія: 6-ступенчатая автомат, ALLGRIP 4WD: 1. Режим Auto; 2. Режим Sport',
-      price: 528000,
-      photo: "./assets/Jimny.png",
-      markId: 100
-  },
-  { 
-    id:33, 
-    model: 'Susuki VitaraV2', 
-    description: 'ALLGRIP 4WD:Система повного привода , Трансмісія: 6-ступенчатая автомат, ALLGRIP 4WD: 1. Режим Auto; 2. Режим Sport',
-    price: 528760,
-    photo: "./assets/VitaraS2.png",
-    markId: 100
-}
-  ])); 
+    localStorage.setItem('car', JSON.stringify([
+      { 
+        id:32, 
+        model: 'Susuki VitaraV2', 
+        description: 'ALLGRIP 4WD:Система повного привода , Трансмісія: 6-ступенчатая автомат, ALLGRIP 4WD: 1. Режим Auto; 2. Режим Sport',
+        price: 528000,
+        photo: "./assets/Jimny.png",
+        markId: 100,
+        quantity: 0
+      },
+      { 
+        id:33, 
+        model: 'Susuki VitaraV3', 
+        description: 'ALLGRIP 4WD:Система повного привода , Трансмісія: 6-ступенчатая автомат, ALLGRIP 4WD: 1. Режим Auto; 2. Режим Sport',
+        price: 528760,
+        photo: "./assets/VitaraS2.png",
+        markId: 100,
+        quantity: 0
+      }
+    ])); 
   }
 
   ngOnInit() {
     this.arr = JSON.parse(localStorage.getItem('car'));
   }
 
-  total(){
-
+  clear(){
+    let li = document.getElementsByClassName('chartList');
+    let div = document.getElementById('chart');
+    localStorage.clear();
+    div.parentNode.removeChild(div);
+  }
+  quantityMinus(chart){
+    if(chart.quantity > 1){
+      this.total -= chart.price; 
+      chart.quantity -= 1;
+    }
+  }
+  quantityPlusp(chart){
+    chart.quantity += 1;
+    this.total += chart.price;
+  }
+  delItem(chart){    
+    let id = document.getElementById(chart.id);
+    if(this.arr.find(x => x.id == chart.id)){
+      localStorage.removeItem('car');
+      id.parentNode.removeChild(id);
+      this.total -= chart.price * chart.quantity; 
+    }
+  }
+  goToOrder(){
+    location.replace('order');
+    localStorage.setItem('buy', JSON.stringify(this.total));
   }
 } 
