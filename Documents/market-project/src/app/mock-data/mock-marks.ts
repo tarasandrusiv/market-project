0120a39a23e66adaacd8f864656ef19e739b7589
Apple Macbook <tarasandrusivkb24@gmail.com>
2019-02-19 20:05:53 +0200

    updated header&footer

--- Documents/market-project/src/app/mock-data/mock-marks.ts
+++ Documents/market-project/src/app/mock-data/mock-marks.ts
@@ -3,5 +3,5 @@ import { Mark } from "../models/mark";
 export const MARKS: Mark[] = [
     {id: 100,title: 'Suzuki'},
     {id: 200,title: 'BMV'},
-    { id: 300,title: 'VW'}
+    {id: 300,title: 'VW'}
 ];
