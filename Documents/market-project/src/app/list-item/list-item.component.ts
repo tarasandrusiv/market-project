f786d46b42add8534c2d440f95ad8e97faf9ac5e
FantomAS527249 <zelisko.mykhaylo@gmail.com>
2019-02-20 19:52:07 +0200

    no message
    Signed-off-by: FantomAS527249 <zelisko.mykhaylo@gmail.com>

--- Documents/market-project/src/app/list-item/list-item.component.ts
+++ Documents/market-project/src/app/list-item/list-item.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CARS } from '../mock-data/mock-cars';
+import { MARKS } from '../mock-data/mock-marks';
+import { Car } from '../models/car';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-list-item',
@@ -6,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-item.component.css']
 })
 export class ListItemComponent implements OnInit {
+  marks = MARKS;
+  cars = CARS;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.getCar();
+  }
+
+  getCar() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    console.log(id)
   }
 
 }
--- Documents/market-project/src/app/list/list.component.ts
+++ Documents/market-project/src/app/list/list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CARS } from '../mock-data/mock-cars';
 import { MARKS } from '../mock-data/mock-marks';
+import { Car } from '../models/car';
 
 @Component({
   selector: 'app-list',
@@ -10,10 +11,20 @@ import { MARKS } from '../mock-data/mock-marks';
 export class ListComponent implements OnInit {
   marks = MARKS;
   cars = CARS;
+  filteredCars;
 
   constructor() { }
 
   ngOnInit() {
+    this.filteredCars = this.cars;
   }
+
+  details(car: Car) {
+    location.replace("/item/" + car.id);
+  }
+  sort(markId: number) {
+    this.filteredCars = markId ? this.cars.filter(x=> x.markId === markId) : this.cars;
+  }
+
 
 }
