a81ab4f714bc3063395bd921d70601c84b9dae86
NikolayBlackJack <44206804+NikolayBlackJack@users.noreply.github.com>
2019-02-21 14:43:06 +0200

    ///

--- Documents/market-project/src/app/list/list.component.ts
+++ Documents/market-project/src/app/list/list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CARS } from '../mock-data/mock-cars';
+import { MARKS } from '../mock-data/mock-marks';
+import { Car } from '../models/car';
 
 @Component({
   selector: 'app-list',
@@ -7,29 +9,22 @@ import { CARS } from '../mock-data/mock-cars';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  marks = MARKS;
   cars = CARS;
-
-  
-  
-  constructor() { 
-    
-  }
+  filteredCars;
+  constructor() { }
 
   ngOnInit() {
+    this.filteredCars = this.cars;
+  }
+
+  details(car: Car) {
+    location.replace("/item/" + car.id);
+  }
+  sort(markId: number) {
+    this.filteredCars = markId ? this.cars.filter(x=> x.markId === markId) : this.cars;
     
   }
-  // addChart(car){
-  //   let arr = JSON.parse(localStorage.getItem('car'));
-  //   console.log(arr)
-  //   if(arr) {
-  //     if(arr.find(x=> x.id === car.id)) return
-  //     arr.push(car)
-  //     localStorage.setItem('car', JSON.stringify(arr));
-  //   }else{
-  //     localStorage.setItem('car', JSON.stringify([car]));
-  //   }
 
-  // }
-  
-  
+
 }
