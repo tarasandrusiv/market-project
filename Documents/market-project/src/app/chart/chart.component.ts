4b30022aea6fa5dbf5044d7b701c98d76575b371
NikolayBlackJack <nikola716@gmail.com>
2019-02-26 21:47:11 +0200

    marge

--- Documents/market-project/src/app/chart/chart.component.ts
+++ Documents/market-project/src/app/chart/chart.component.ts
@@ -15,11 +15,61 @@ export class ChartComponent implements OnInit {
   cars = CARS;
   len = this.cars.length;
   arr: any;
+<<<<<<< HEAD
   total = 0; 
   constructor(
     private router:Router,
     private carSer: CarService
   ) {}
+=======
+  total = 0;
+  
+  
+  constructor(
+    private router:Router,
+    private carSer: CarService
+  ) { 
+    
+    localStorage.setItem('car', JSON.stringify([
+  { 
+      id: 11, 
+      model: "Golf", 
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+      price: 579891,
+      photo: "./assets/Golf.png",
+      markId: 300,
+      quantity: 1
+  },
+  { 
+      id: 12, 
+      model: "Passat", 
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+      price: 878302,
+      photo: "./assets/Passat.png",
+      markId: 300,
+      quantity: 1
+  },
+  { 
+      id: 13, 
+      model: "Polo sedan", 
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+      price: 374679,
+      photo: "./assets/Polo-sedan.png",
+      markId: 300,
+      quantity: 1
+  },
+  { 
+      id: 14, 
+      model: "Touareg", 
+      description: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.",
+      price: 1673219,
+      photo: "./assets/Touareg.png",
+      markId: 300,
+      quantity: 1
+  }
+    ])); 
+  }
+>>>>>>> 5f3545263be2e31e7531ab1893519dd4b6a0d6ae
 
   ngOnInit() {
     this.arr = JSON.parse(localStorage.getItem('car'));
@@ -36,24 +86,35 @@ export class ChartComponent implements OnInit {
  
   delItem(chart){   
     this.arr = this.arr.filter(arr => arr != chart); 
+<<<<<<< HEAD
     localStorage.setItem('car', JSON.stringify(this.arr));
     this.calculateTotal();
     this.setCount();
+=======
+    localStorage.setItem('car', this.arr);
+    this.setCount();
+    this.calculateTotal();
+>>>>>>> 5f3545263be2e31e7531ab1893519dd4b6a0d6ae
     return this.arr;
     
   }
   calculateTotal(){
     this.total = 0;
+<<<<<<< HEAD
     if(this.arr){
       this.arr.map(x=> this.total += x.price * x.quantity);
       localStorage.setItem('car', JSON.stringify(this.arr)) 
     }
     this.setCount();
+=======
+    this.arr.map(x=> this.total += x.price * x.quantity); 
+>>>>>>> 5f3545263be2e31e7531ab1893519dd4b6a0d6ae
   }
   goToOrder(){
     this.router.navigateByUrl('/order');
   }
   link(chart){
+<<<<<<< HEAD
     this.router.navigateByUrl(`item/${chart.id}`)
   }
   setCount(){
@@ -62,5 +123,17 @@ export class ChartComponent implements OnInit {
       this.arr.forEach(x=> count += x.quantity);
     }
     this.carSer.count.next(count);
+=======
+    this.router.navigateByUrl('item/' + chart.id)
+  }
+  setCount(){
+    let count;
+    if(this.arr != undefined){
+       count = this.arr.length;
+    }else{
+      count = 0;
+    }
+    this.carSer.setCount(count);
+>>>>>>> 5f3545263be2e31e7531ab1893519dd4b6a0d6ae
   }
 } 
--- Documents/market-project/src/app/list/list.component.ts
+++ Documents/market-project/src/app/list/list.component.ts
@@ -2,9 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CARS } from '../mock-data/mock-cars';
 import { MARKS } from '../mock-data/mock-marks';
 import { Car } from '../models/car';
+<<<<<<< HEAD
 import { ActivatedRoute, Router } from '@angular/router';
 
 
+=======
+>>>>>>> 5f3545263be2e31e7531ab1893519dd4b6a0d6ae
 
 @Component({
   selector: 'app-list',
@@ -15,6 +18,7 @@ export class ListComponent implements OnInit {
   marks = MARKS;
   cars = CARS;
   filteredCars;
+<<<<<<< HEAD
   constructor(
     private route: ActivatedRoute, 
     private router: Router
@@ -31,3 +35,21 @@ export class ListComponent implements OnInit {
   
 }
 
+=======
+  constructor() { }
+
+  ngOnInit() {
+    this.filteredCars = this.cars;
+  }
+
+  details(car: Car) {
+    location.replace("/item/" + car.id);
+  }
+  sort(markId: number) {
+    this.filteredCars = markId ? this.cars.filter(x=> x.markId === markId) : this.cars;
+    
+  }
+
+
+}
+>>>>>>> 5f3545263be2e31e7531ab1893519dd4b6a0d6ae
